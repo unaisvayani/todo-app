d671b0a30d4943575b13de20039e93236a28aa20
Unais Vayani <unaisvayani26@gmail.com>
2021-07-25 05:08:20 +0500

    Component Testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { TextField } from "@material-ui/core";
+import { TableRow as Row } from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
+      <form noValidate autoComplete="off">
+        <Row>
+          <TextField id="standard-basic" label="Standard" />
+        </Row>
+        <Row>
+          <TextField id="filled-basic" label="Filled" variant="filled" />
+        </Row>
+        <Row>
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </Row>
+      </form>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
